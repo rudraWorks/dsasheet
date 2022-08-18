4f8afc1d42d988068aa92d654ff102b867b47862
rudra pratap singh <63004704+rudraWorks@users.noreply.github.com>
2022-08-18 23:00:31 +0530

    backend

--- controllers/graphController.js
+++ controllers/graphController.js
@@ -16,77 +16,77 @@ module.exports.ch3 = (req,res) =>{
 }
 
 
-// module.exports.ch4 = (req,res) =>{
-//     return res.render('graph/ch4')
-// }
+module.exports.ch4 = (req,res) =>{
+    return res.render('graph/ch4')
+}
 
-// module.exports.ch5 = (req,res) =>{
-//     return res.render('graph/ch5')
-// }
+module.exports.ch5 = (req,res) =>{
+    return res.render('graph/ch5')
+}
 
-// module.exports.ch6 = (req,res) =>{
-//     return res.render('graph/ch6')
-// }
+module.exports.ch6 = (req,res) =>{
+    return res.render('graph/ch6')
+}
 
-// module.exports.ch7 = (req,res) =>{
-//     return res.render('graph/ch7')
-// }
+module.exports.ch7 = (req,res) =>{
+    return res.render('graph/ch7')
+}
 
-// module.exports.ch8 = (req,res) =>{
-//     return res.render('graph/ch8')
-// }
+module.exports.ch8 = (req,res) =>{
+    return res.render('graph/ch8')
+}
 
-// module.exports.ch9 = (req,res) =>{
-//     return res.render('graph/ch9')
-// }
+module.exports.ch9 = (req,res) =>{
+    return res.render('graph/ch9')
+}
 
-// module.exports.ch10 = (req,res) =>{
-//     return res.render('graph/ch10')
-// }
+module.exports.ch10 = (req,res) =>{
+    return res.render('graph/ch10')
+}
 
-// module.exports.ch11 = (req,res) =>{
-//     return res.render('graph/ch11')
-// }
+module.exports.ch11 = (req,res) =>{
+    return res.render('graph/ch11')
+}
 
-// module.exports.ch12 = (req,res) =>{
-//     return res.render('graph/ch12')
-// }
+module.exports.ch12 = (req,res) =>{
+    return res.render('graph/ch12')
+}
 
-// module.exports.ch13 = (req,res) =>{
-//     return res.render('graph/ch13')
-// }
+module.exports.ch13 = (req,res) =>{
+    return res.render('graph/ch13')
+}
 
-// module.exports.ch14 = (req,res) =>{
-//     return res.render('graph/ch14')
-// }
+module.exports.ch14 = (req,res) =>{
+    return res.render('graph/ch14')
+}
 
-// module.exports.ch15 = (req,res) =>{
-//     return res.render('graph/ch15')
-// }
+module.exports.ch15 = (req,res) =>{
+    return res.render('graph/ch15')
+}
 
-// module.exports.ch16 = (req,res) =>{
-//     return res.render('graph/ch16')
-// }
+module.exports.ch16 = (req,res) =>{
+    return res.render('graph/ch16')
+}
 
-// module.exports.ch17 = (req,res) =>{
-//     return res.render('graph/ch17')
-// }
+module.exports.ch17 = (req,res) =>{
+    return res.render('graph/ch17')
+}
 
-// module.exports.ch18 = (req,res) =>{
-//     return res.render('graph/ch18')
-// }
+module.exports.ch18 = (req,res) =>{
+    return res.render('graph/ch18')
+}
 
-// module.exports.ch19 = (req,res) =>{
-//     return res.render('graph/ch19')
-// }
+module.exports.ch19 = (req,res) =>{
+    return res.render('graph/ch19')
+}
 
-// module.exports.ch20 = (req,res) =>{
-//     return res.render('graph/ch20')
-// }
+module.exports.ch20 = (req,res) =>{
+    return res.render('graph/ch20')
+}
 
-// module.exports.ch21 = (req,res) =>{
-//     return res.render('graph/ch21')
-// }
+module.exports.ch21 = (req,res) =>{
+    return res.render('graph/ch21')
+}
 
 
 
